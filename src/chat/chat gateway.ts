6cefdafab9b489dfb7dd0e3f16114fe1cedccef7
aminvv <aminviper1378@gmail.com>
2025-01-25 12:06:04 +0330

    How to communicate in a socket---------->>>>>>(chapter:10 section:3)

--- src/chat/chat gateway.ts
+++ src/chat/chat gateway.ts
@@ -1,20 +1,27 @@
-import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway } from "@nestjs/websockets";
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from "socket.io";
 
 @WebSocketGateway({
     cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
+        origin: "*",
+        methods: ["GET", "POST"],
     },
-  })
+})
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer() server: Server
     afterInit(server: any) {
         console.log("socket initialized");
-        
+
     }
     handleConnection(client: any, ...args: any[]) {
+        const { sockets } = this.server.sockets
+        console.log("clientId:" + client.id + "connected")
+        console.log("Online User" + sockets.size);
 
     }
     handleDisconnect(client: any) {
-
+        const { sockets } = this.server.sockets
+        console.log("clientId:" + client.id + "disconnected")
+        console.log("Online User" + sockets.size);
     }
 }
