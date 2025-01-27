fb51f0ba09873f317e763fcb8f03619fc5a596d1
aminvv <aminviper1378@gmail.com>
2025-01-27 15:42:45 +0330

    chat room end section---------->>>>>>>>(chapter:10 section:8)

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,35 @@
+let messages = [];
 const socket=io("http://localhost:3000")
 
 socket.on("connect",()=>{
   console.log("connected");
   
 })
-socket.on("client-chat",(data)=>{
-  console.log("client data",data);
-  
-})
+
 socket.on("exception",(data)=>{
   console.log(data);
   
 })
+
+
+socket.on("client-chat", data => {
+  const {user, roomName, time, message: msg} = data;
+  const message = `
+  <div class=${username === user.username ? "outgoing-chats" : "received-chats"}>
+       <div class=${username === user.username ? "outgoing-chats-img" : "received-chats-img"}>
+            <img src=${username === user.username ? "user1.png" : "user2.png"} />
+        </div>
+        <div class=${username === user.username ? "outgoing-msg" : "received-msg"}>
+          <div class=${username === user.username ? "outgoing-chats-msg" : "received-chats-msg"}>
+            <p class="multi-msg">${msg}</p>
+    <span class="time">${time}</span>
+          </div >
+        </div >
+    </div >
+    `;
+  messages.push(message);
+  document.getElementById("msg-page").innerHTML = messages.join("");
+});
 
 const username=prompt("enter username","amin")
 const roomName=prompt("enter room name","nodejs")
